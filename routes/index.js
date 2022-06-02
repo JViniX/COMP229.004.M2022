58bc41de18841112a719b8d76e161954126f385d
JViniX <47263194+JViniX@users.noreply.github.com>
2022-06-01 20:29:37 -0400

    New Nav bar

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', home);
+
+function home(req, res, next) {
   res.render('index', { 
     title: 'Home',
     userName: 'Julio'  });
-});
+}
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
