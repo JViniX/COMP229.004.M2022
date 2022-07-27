113eb7fea1d652093a6004b8e2abb6f73bea11ca
JViniX <47263194+JViniX@users.noreply.github.com>
2022-07-27 19:36:31 -0400

    Week11 - dotenv

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
-// Alert: For production environment never expose your connection string AND secret keys.
+require('dotenv').config()
+
 module.exports = {
-    "ATLASDB" : "mongodb+srv://appuser:xhFYadTL1HYRH1tU@cluster004m2022.aydiv.mongodb.net/inventory?retryWrites=true&w=majority",
-    "SECRETKEY" : "cRfUjXn2r5u8x/A?D(G-KaPdSgVkYp3s6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-"
+    "ATLASDB" : process.env.ATLASDB,
+    "SECRETKEY" : process.env.SECRETKEY
 }
